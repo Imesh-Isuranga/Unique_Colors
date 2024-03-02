9af57ad6262350a088ecd189767098e44fb693b6
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-03-03 01:18:47 +0530

    Exposed category Ids

--- Angular-Frontend/Uniquecolors-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Angular-Frontend/Uniquecolors-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,15 +11,28 @@ import { Product } from '../../common/product';
 export class ProductListComponent implements OnInit{
 
   products:Product[]=[];
+  currentCategoryId:number=1;
 
-  constructor(private productServices:ProductService){}
+  constructor(private productServices:ProductService,
+              private route:ActivatedRoute){}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(()=>{
+      this.listProducts();
+    });
   }
 
   listProducts(){
-    this.productServices.getProductList().subscribe(
+
+    const hasCategoryId:boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }else{
+      this.currentCategoryId = 1;
+    }
+
+    this.productServices.getProductList(this.currentCategoryId).subscribe(
       data=>{
         this.products=data;
     })
--- Spring-Boot-Backend/unique_colors/src/main/java/com/unique_colors/ecommerce/config/MyDataRestConfig.java
+++ Spring-Boot-Backend/unique_colors/src/main/java/com/unique_colors/ecommerce/config/MyDataRestConfig.java
@@ -2,6 +2,8 @@ package com.unique_colors.ecommerce.config;
 
 import com.unique_colors.ecommerce.entity.Product;
 import com.unique_colors.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -10,8 +12,20 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
@@ -28,5 +42,21 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
